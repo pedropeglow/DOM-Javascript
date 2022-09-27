d78c236fe6a1d9f1fe053c0433618bcd8984bc87
pedropeglow <pedropeglowm@gmail.com>
2022-09-27 17:32:36 -0300

    :sparkles: Aplicando JavaScript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const robotron = document.querySelector("#robotron");
+robotron.addEventListener("click", () => {});
+
+function dizOi(nome) {
+  console.log("oi " + nome);
+  console.log("Bem-vindo ao Robotron 2000");
+}
+
+dizOi("Pedro");
